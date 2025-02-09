94f0ce245a47c22fc106150de5c4c4fa194f0862
SimHanKiong <simhankiong@gmail.com>
2025-02-09 23:02:42 +0800

    Change env location

--- app/core/config.py
+++ app/core/config.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file="app/.env")
+    model_config = SettingsConfigDict(env_file=".env")
 
     POSTGRES_SERVER: str
     POSTGRES_PORT: int
