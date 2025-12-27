4c86f4e9f6e3cc00ff64aed9fcc21128c8cd9a83
Han Kiong Sim <hankiongsim@HKs-MacBook.local>
2025-12-27 20:15:19 +0800

    Fix parent create

--- app/services/parent.py
+++ app/services/parent.py
@@ -13,13 +13,8 @@ def create_parent(db: Session, parent_in: ParentCreate) -> ParentOutWithChildren
         raise IntegrityException("Enquiry")
 
     parent = ParentBase(
+        **parent_in.model_dump(),
         email=enquiry.email,
-        contact1_name=parent_in.contact1_name,
-        contact1_no=parent_in.contact1_no,
-        contact1_relationship=parent_in.contact1_relationship,
-        contact2_name=parent_in.contact2_name,
-        contact2_no=parent_in.contact2_no,
-        contact2_relationship=parent_in.contact2_relationship,
         home_postal_code=enquiry.home_postal_code,
         home_unit_no=enquiry.home_unit_no,
         home_address=enquiry.home_address,
@@ -27,23 +22,13 @@ def create_parent(db: Session, parent_in: ParentCreate) -> ParentOutWithChildren
         am_address=enquiry.am_address,
         pm_postal_code=enquiry.pm_postal_code,
         pm_address=enquiry.pm_address,
-        under_fas=parent_in.under_fas,
         fare=enquiry.fare,
-        enquiry_id=enquiry.id,
     )
     parent = parent_crud.create(db, parent)
 
     for child_in in parent_in.children:
         student = StudentBase(
-            full_name=child_in.full_name,
-            given_name=child_in.given_name,
-            gender=child_in.gender,
-            level=child_in.level,
-            class_name=child_in.class_name,
-            date_of_birth=child_in.date_of_birth,
-            nric=child_in.nric,
-            transport_start_date=child_in.transport_start_date,
-            transport_requirement=child_in.transport_requirement,
+            **child_in.model_dump(),
             block=enquiry.block,
             status="",
             school_id=enquiry.school_id,
